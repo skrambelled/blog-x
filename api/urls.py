fc63efd5b9674d3b66a05d9ecd889671b7eaaa20
Mark Bell <skrambelled@gmail.com>
2020-12-29 16:05:30 -0800

    changed my api/v1/create/ endpoint to just /api/v1/

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import PostAPIListCreateView, PostAPIDetailUpdateView, PostAPIDetailDeleteView
 
 urlpatterns = [
-    path('create/', PostAPIListCreateView.as_view(), name='api/v1/create'),
+    path('', PostAPIListCreateView.as_view(), name='api/v1/create'),
     path('update/<int:pk>', PostAPIDetailUpdateView.as_view(), name='api/v1/update'),
     path('delete/<int:pk>', PostAPIDetailDeleteView.as_view(), name='api/v1/delete')
 ]
